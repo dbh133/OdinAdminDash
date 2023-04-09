eb432142ecf822f14eb71f8b1ab4389b979d54ba
Dylan Hull <1429003+dbh133@users.noreply.github.com>
2023-04-09 11:58:51 -0400

    added search bar and icon

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const LOGO = document.querySelector('side-content');
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+
+// Draw a circle
+ctx.fillStyle = 'lightgrey';
+ctx.beginPath();
+ctx.arc(25, 25, 20, 0, 2 * Math.PI);
+ctx.fill();
+
+// Draw a rectangle inside the circle, reduced by 10%
+ctx.fillStyle = 'rgba(39, 121, 228, 0.993)';
+ctx.fillRect(13, 13, 24, 24);
+
+// Draw a triangle inside the rectangle, reduced by 10%
+ctx.fillStyle = 'lightgrey';
+ctx.beginPath();
+ctx.moveTo(25, 14.3);
+ctx.lineTo(34.4, 30.4);
+ctx.lineTo(15.6, 30.4);
+ctx.closePath();
+ctx.fill();
